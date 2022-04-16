6534e11df6baa15a1cf88fb22afc46f21309efee
SlaVistaPL <yt.slavistapl@gmail.com>
2022-04-16 10:04:19 +0200

    completion of the codebase

--- events/ready.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'ready',
-    once: true,
-    async execute() {
-        console.log("Borealis is ready to go!");
-    }
-}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 /*
 * Slash commands handler for Borealis bot.
+* Check out '/classes/borealis.ts' file for full source code of the handler.
 */
 
 import Borealis from './classes/borealis';
